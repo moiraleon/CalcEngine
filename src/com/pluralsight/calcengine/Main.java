85855484aa5f31ccc07929bb47fd2ac22f62ec5b
Moira Leon <moira.leon2@gmail.com>
2021-08-17 16:48:12 -0600

    adding a switch statement

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -4,21 +4,48 @@ public class Main {
 
     public static void main(String[] args) {
 	double value1 = 100.0d;
-	double value2 = 50.0d;
+	double value2 = 0.0d;
 	double result = 0.0d;
 	char opCode = 'd';
 
-	if(opCode =='a')
-	    result = value1 + value2;
-	else if(opCode =='s')
-	    result = value1 - value2;
-	else if(opCode =='m')
-	    result = value1 * value2;
-	else if(opCode =='d')
-	    result =value1 / value2;
-	else
-	    result = 0.0d;
+	//adding a switch statement
+	switch (opCode){
+		case 'a':
+			result = value1 + value2;
+			break;
+		case 's':
+			result = value1 - value2;
+			break;
+		case 'm':
+			result = value1 * value2;
+			break;
+		case 'd':
+//			if(value2 !=0)
+//			result = value1 / value2;
+//			break; or for applying conditional assignment
+			result =value2 !=0 ? value1/value2: 0.0d;
+			break;
+		default:
+			System.out.println("Invalid opCode"+opCode);
+			result = 0.0d;
+			break;
+	}
 
+
+//	if(opCode =='a')
+//	    result = value1 + value2;
+//	else if(opCode =='s')
+//	    result = value1 - value2;
+//	else if(opCode =='m')
+//	    result = value1 * value2;
+//	else if(opCode =='d') {
+//		if(value2 !=0)
+//		result = value1 / value2;
+//	}
+//	else {
+//		System.out.println("Invalid opCode"+opCode);
+//		result = 0.0d;
+//	}
 	System.out.println(result);
     }
 }
