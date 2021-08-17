2e0a93654c5f63ab843767fe7b43c85efd2536ad
Moira Leon <moira.leon2@gmail.com>
2021-08-17 17:48:40 -0600

    arrays, for loop, and for each loop added

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -2,34 +2,65 @@ package com.pluralsight.calcengine;
 
 public class Main {
 
-    public static void main(String[] args) {
-	double value1 = 100.0d;
-	double value2 = 0.0d;
-	double result = 0.0d;
-	char opCode = 'd';
-
-	//adding a switch statement
-	switch (opCode){
-		case 'a':
-			result = value1 + value2;
-			break;
-		case 's':
-			result = value1 - value2;
-			break;
-		case 'm':
-			result = value1 * value2;
-			break;
-		case 'd':
+	public static void main(String[]args){
+//adding an array of data
+	double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+	double[] rightVals ={50.0d,92.0d,17.0d,3.0d};
+	char[] opCodes ={'d','a','s','m'};
+	double[] results = new double[opCodes.length];
+//creating array for loop
+	for (int i =0; i<opCodes.length; i++){
+		switch (opCodes[i]) {
+			case 'a':
+				results[i] = leftVals[i] + rightVals[i];
+				break;
+			case 's':
+				results[i] = leftVals[i] - rightVals[i];
+				break;
+			case 'm':
+				results[i] = leftVals[i] * rightVals[i];
+				break;
+			case 'd':
 //			if(value2 !=0)
 //			result = value1 / value2;
 //			break; or for applying conditional assignment
-			result =value2 !=0 ? value1/value2: 0.0d;
-			break;
-		default:
-			System.out.println("Invalid opCode"+opCode);
-			result = 0.0d;
-			break;
+				results[i] = rightVals[i]!= 0 ? leftVals[i] / rightVals[i] : 0.0d;
+				break;
+			default:
+				System.out.println("Invalid opCode" + opCodes[i]);
+				results[i] = 0.0d;
+				break;
+		}
+
 	}
+//    public static void main(String[] args) {
+//	double value1 = 100.0d;
+//	double value2 = 0.0d;
+//	double result = 0.0d;
+//	char opCode = 'd';
+
+	//adding a switch statement
+//	switch (opCode){
+//		case 'a':
+//			result = value1 + value2;
+//			break;
+//		case 's':
+//			result = value1 - value2;
+//			break;
+//		case 'm':
+//			result = value1 * value2;
+//			break;
+//		case 'd':
+////			if(value2 !=0)
+////			result = value1 / value2;
+////			break; or for applying conditional assignment
+//			result =value2 !=0 ? value1/value2: 0.0d;
+//			break;
+//		default:
+//			System.out.println("Invalid opCode"+opCode);
+//			result = 0.0d;
+//			break;
+//	}
 
 
 //	if(opCode =='a')
@@ -46,6 +77,10 @@ public class Main {
 //		System.out.println("Invalid opCode"+opCode);
 //		result = 0.0d;
 //	}
-	System.out.println(result);
+		//adding a for each loop
+
+		for(double currentResult: results)
+	System.out.println(currentResult);
     }
 }
+
