a1076c75eda267e02040dafce7ce87e05fce5dc4
Moira Leon <moira.leon2@gmail.com>
2021-08-18 13:13:43 -0600

    adding a method

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -10,27 +10,28 @@ public class Main {
 	double[] results = new double[opCodes.length];
 //creating array for loop
 	for (int i =0; i<opCodes.length; i++){
-		switch (opCodes[i]) {
-			case 'a':
-				results[i] = leftVals[i] + rightVals[i];
-				break;
-			case 's':
-				results[i] = leftVals[i] - rightVals[i];
-				break;
-			case 'm':
-				results[i] = leftVals[i] * rightVals[i];
-				break;
-			case 'd':
-//			if(value2 !=0)
-//			result = value1 / value2;
-//			break; or for applying conditional assignment
-				results[i] = rightVals[i]!= 0 ? leftVals[i] / rightVals[i] : 0.0d;
-				break;
-			default:
-				System.out.println("Invalid opCode" + opCodes[i]);
-				results[i] = 0.0d;
-				break;
-		}
+		results[i] = execute(opCodes[i], leftVals[i],rightVals[i]);
+//		switch (opCodes[i]) {
+//			case 'a':
+//				results[i] = leftVals[i] + rightVals[i];
+//				break;
+//			case 's':
+//				results[i] = leftVals[i] - rightVals[i];
+//				break;
+//			case 'm':
+//				results[i] = leftVals[i] * rightVals[i];
+//				break;
+//			case 'd':
+////			if(value2 !=0)
+////			result = value1 / value2;
+////			break; or for applying conditional assignment
+//				results[i] = rightVals[i]!= 0 ? leftVals[i] / rightVals[i] : 0.0d;
+//				break;
+//			default:
+//				System.out.println("Invalid opCode" + opCodes[i]);
+//				results[i] = 0.0d;
+//				break;
+//		}
 
 	}
 //    public static void main(String[] args) {
@@ -82,5 +83,30 @@ public class Main {
 		for(double currentResult: results)
 	System.out.println(currentResult);
     }
+	//adding a method
+	static double execute(char opCode, double leftVal, double rightVal){
+		double result;
+		switch (opCode) {
+			case 'a':
+				result = leftVal+ rightVal;
+				break;
+			case 's':
+				result = leftVal- rightVal;
+				break;
+			case 'm':
+				result = leftVal * rightVal;
+				break;
+			case 'd':
+				result = rightVal!= 0 ? leftVal / rightVal: 0.0d;
+				break;
+			default:
+				System.out.println("Invalid opCode" + opCode;
+				result = 0.0d;
+				break;
+		}
+		return result;
+	}
 }
+
+
 
