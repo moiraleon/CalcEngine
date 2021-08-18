f49d1ad8f2fdf1903f8fb7f9ee968f32c3918582
Moira Leon <moira.leon2@gmail.com>
2021-08-18 15:39:15 -0600

    interactive

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -1,5 +1,7 @@
 package com.pluralsight.calcengine;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[]args) {
@@ -86,10 +88,29 @@ public class Main {
 
 			for (double currentResult : results)
 				System.out.println(currentResult);
-		} else if (args.length ==3)
+		} else if(args.length ==1 && args[0].equals("interactive"))
+			executeInteractively();
+		else if (args.length ==3)
 			handleCommandLine(args);
 		else
 			System.out.println("Please provide an operation code and 2 numeric values");
+	}
+		static void executeInteractively(){
+
+		System.out.println("Enter an operation and two numbers");
+			Scanner scanner = new Scanner(System.in);
+			String userInput = scanner.nextLine();
+			String[] parts = userInput.split(" ");
+			performOperation(parts);
+
+		}
+
+	private static void performOperation(String[] parts) {
+		char opCode = opCodeFromString(parts[0]);
+		double leftVal = valueFromWord(parts[1]);
+		double rightVal = valueFromWord(parts[2]);
+		double result = execute(opCode, leftVal,rightVal);
+		System.out.println(result);
 	}
 
 	private static void handleCommandLine(String[] args) {
@@ -117,11 +138,29 @@ public class Main {
 				result = rightVal!= 0 ? leftVal / rightVal: 0.0d;
 				break;
 			default:
-				System.out.println("Invalid opCode" + opCode;
+				System.out.println("Invalid opCode" + opCode);
 				result = 0.0d;
 				break;
 		}
 		return result;
+	}
+
+	static char opCodeFromString(String operationName){
+		char opCode = operationName.charAt(0);
+		return opCode;
+	}
+	static double valueFromWord(String word){
+		String [] numberWords = {
+		"zero","one","two","three","four","five","six","seven","eight","nine"
+		};
+		double value = 0d;
+		for(int index =0; index< numberWords.length; index++){
+			if (word.equals(numberWords[index])){
+				value = index;
+				break;
+			}
+		}
+		return value;
 	}
 }
 
